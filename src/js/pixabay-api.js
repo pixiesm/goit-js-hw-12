cf825e6052e067dd96b0296b01e182fcdb6823ba
Smakovska Yuliia <juliasmakovskaa@gmail.com>
2024-08-09 00:46:55 +0300

    did 45 %

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,40 @@
 // key: 44685360-74500565ee10f6b109a60bbe8
+import axios from "axios";
+axios.defaults.baseURL = "https://pixabay.com/";
+// axios.defaults.params = {API_KEY : '44685360-74500565ee10f6b109a60bbe8'}
 
 
-export function getImage(image) {
-    const URL = 'https://pixabay.com/api/';
-    const API_KEY = '44685360-74500565ee10f6b109a60bbe8';
-    const params = new URLSearchParams({
+export async function getImage(image, currentPage) {
+    // const URL = 'https://pixabay.com/api/';
+    // const API_KEY = '44685360-74500565ee10f6b109a60bbe8';
+    const params = {
+        key: '44685360-74500565ee10f6b109a60bbe8',
         q: image,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-    });
-    const url = `${URL}?key=${API_KEY}&${params}`;
-    return fetch(url).then((response) => {
-        if (!response.ok) { throw new Error(response.status); }
-        return response.json();
-    }).catch((error) => {
-        iziToast.error({
-          position: "topRight",
-            message: `${error}`,
-        });
-      });
+        page: currentPage,
+        per_page: 15,
+    };
+
+    // const url = `${URL}?${params}`;
+    const response = await axios.get('api/', { params });
+    
+    return response.data;
+   
+   
+    // fetch(url).then((response) => {
+    //     if (!response.ok) { throw new Error(response.status); }
+    //     return response.json();
+    // }).catch((error) => {
+    //     iziToast.error({
+    //       position: "topRight",
+    //         message: `${error}`,
+    //     });
+    //   });
         
         
         // data => data.json());
     
 };
+    
--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@
 import { getImage } from "./js/pixabay-api";
 // getImage("cat").then((data) => { console.log(data) }).catch((error) => { console.log(error) });
 
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css"
 
+import axios from "axios";
+
 import {
     markupImages,
-    // showLoader,
-    // hideLoader
 } from "./js/render-functions";
+
+
 
 export const refs = { input: document.querySelector('.form-input'),
 btn: document.querySelector('.search-btn'),
@@ -18,43 +21,54 @@ btn: document.querySelector('.search-btn'),
     picturesList: document.querySelector('.gallery-list'),
 loader: document.querySelector('.loader')
 };
-function showLoader() { refs.loader.classList.remove('hidden'); }
-function hideLoader() { refs.loader.classList.add('hidden'); }
-refs.searchForm.addEventListener('submit', e => {
+// function showLoader() { refs.loader.classList.remove('hidden'); }
+// function hideLoader() { refs.loader.classList.add('hidden'); }
+
+let photosName;
+let currentPage = 1;
+
+refs.searchForm.addEventListener('submit', async e => {
     e.preventDefault();
-    const photosName = refs.input.value.trim();
+    photosName = refs.input.value.trim();
     if (photosName === '') {
-         iziToast.warning({
+        iziToast.warning({
             title: 'warning',
             message: 'Search field can not be empty!',
             position: 'topRight'
         });
         return;
     }
-    showLoader();
+    // showLoader();
     refs.picturesList.innerHTML = "";
-    getImage(photosName).then(({ hits }) => {
-        if (hits.length === 0) {
-            iziToast.error({
-                title: 'Error',
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                position: 'topRight',
-                displayMode: 'once',
-            })
-            return;
-        }
+    const data =  await getImage(photosName, currentPage);
+    markupImages(data.hits);
+    
+});
+
+
+
+    // .then(({ hits }) => {
+    //     if (hits.length === 0) {
+    //         iziToast.error({
+    //             title: 'Error',
+    //             message: 'Sorry, there are no images matching your search query. Please try again!',
+    //             position: 'topRight',
+    //             displayMode: 'once',
+    //         })
+    //         return;
+    //     }
         // hideLoader()
-        markupImages(hits);
-        resetForm();
+//         markupImages(hits);
+//         resetForm();
 
-    }).catch((err) => {
-        iziToast.error({
-            position: "topRight",
-            message: `${err}`,
-        })
-    }).finally(() => hideLoader());
+//     }).catch((err) => {
+//         iziToast.error({
+//             position: "topRight",
+//             message: `${err}`,
+//         })
+//     }).finally(() => hideLoader());
     
     
 
-})
-function resetForm() { refs.searchForm.reset(); }
+// })
+// function resetForm() { refs.searchForm.reset(); }
